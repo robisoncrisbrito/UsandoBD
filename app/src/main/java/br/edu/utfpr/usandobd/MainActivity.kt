68efd62630dbb96caf6bdb01b5aa0eff80ee1095
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2026-04-30 22:06:05 -0300

    Aplicativo pronto, aguardando apenas o código do sqlite

--- app/src/main/java/br/edu/utfpr/usandobd/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandobd/MainActivity.kt
@@ -1,12 +1,19 @@
 package br.edu.utfpr.usandobd
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etCod: EditText
+    private lateinit var etNome: EditText
+    private lateinit var etTelefone: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        etCod = findViewById(R.id.etCod)
+        etNome = findViewById(R.id.etNome)
+        etTelefone = findViewById(R.id.etTelefone)
+    }
+
+    fun btIncluirOnClick(view: View) {
+
+    }
+    fun btAlterarOnClick(view: View) {
+
+    }
+    fun btExcluirOnClick(view: View) {
+
+    }
+    fun btPesquisarOnClick(view: View) {
+
+    }
+    fun btListarOnClick(view: View) {
+
     }
 }
